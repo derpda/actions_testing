87582111d80c7cf20aadab177436ed36d054cdf6
Peter Spalthoff <peter.spalthoff@gmail.com>
2025-12-08 15:17:49 +0900

    test

---
+++ scripts/dependencies.sh
@@ -0,0 +1,25 @@
+# This scipt is meant to be sourced by other scripts, not executed directly.
+# NOTE: If you change versions here, ensure that you adapt the build.gradle
+#       files of the flutter interface
+#       - sdk/interface/flutter/edgeocr/android/build.gradle
+#       - sdk/interface/flutter/edgeocr/example/android/app/build.gradle
+export JAVA_VERSION=17
+export ANDROID_CMDLINE_TOOLS_VERSION=13114758_latest
+export BUILD_TOOLS_VERSION=36.0.0
+export NDK_VERSION=28.2.13676358
+export CMAKE_VERSION=4.0.3
+export TENSORFLOW_VERSION=2.19.0
+export TFLITE_SUPPORT_VERSION=0.4.4
+export OPENCV_VERSION=4.12.0
+export NLOHMANN_JSON_VERSION=3.12.0
+export MBEDTLS_VERSION=3.6.4
+export CURL_VERSION=8.15.0
+export LIBJPEG_TURBO_VERSION=3.1.1
+export ANDROID_MIN_API_LEVEL=26
+export ANDROID_TARGET_API_LEVEL=36
+export EIGEN_VERSION=3.4.0
+export SWIFT_VERSION=6.1.2
+export FLUTTER_VERSION=3.35.5
+export DART_PUB_COMMIT=c3e50919d11896f014cb971e1776d00a0e2d18b3
+export GH_CLI_VERSION=2.76.2
+export RUST_VERSION=1.88.0
